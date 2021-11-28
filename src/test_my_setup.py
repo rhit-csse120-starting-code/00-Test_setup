7b4050bc501d843d5379ddfbed06d57086fffd45
David Mutchler <mutchler@rose-hulman.edu>
2021-11-28 00:32:28 -0500

    Copied from the master repo to the starting code repo.

---
+++ src/test_my_setup.py
@@ -0,0 +1,8 @@
+print()
+print("In PyCharm:")
+print("  1. Right-click in this window and")
+print("  2. Select:    Run 'test_my_setup'")
+
+print()
+print("If this message shows up in a window,")
+print("your Python setup is OK.")
